e72c9fed572569342fb42a2eab0466d0638dd407
Thanh-Thao HOANG <thanhthaohng@gmail.com>
2018-05-28 20:18:44 +0200

    search Gifs

--- Mangogiphy/app/src/main/java/com/tthaohoang/mangogiphy/MainActivity.kt
+++ Mangogiphy/app/src/main/java/com/tthaohoang/mangogiphy/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.util.Log
 import android.view.View
+import android.widget.SearchView
 import android.widget.Toast
 import com.giphy.sdk.core.models.enums.MediaType
 import com.mikepenz.fastadapter.IAdapter
@@ -14,6 +15,7 @@ import com.tthaohoang.mangogiphy.data.GiphyManager
 import com.tthaohoang.mangogiphy.item.GifItem
 import com.tthaohoang.mangogiphy.model.Gif
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,32 +34,50 @@ class MainActivity : AppCompatActivity() {
         // relier le fastadapter au recycler view
         gifsRecyclerView.adapter = gifsItemAdapter
 
-//      get client from singleton GiphyManager
-        val client = GiphyManager.client
+        searchBar.setIconifiedByDefault(false)
 
+        val client = GiphyManager.client
         var arrayGifs = arrayListOf<Gif>()
 
-        client.search("cats dogs", MediaType.gif, 5, null, null, null) { result, e ->
-            if (result == null) {
-                // Do what you want to do with the error
-            } else {
-                if (result.data != null) {
-                    var content = ""
-                    for (gif in result.data) {
-                        Log.v("giphy", gif.id)
+        searchBar.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.i("tag", query)
+//                on submit, fetch gifs
+                if(query is String) {
 
-                        arrayGifs.add(Gif(gif.images.fixedHeight.gifUrl))
-                    }
+                    gifsItemAdapter.clear()
+                    gifsItemAdapter.notifyDataSetChanged()
 
-                    for(gif in arrayGifs) {
-                        val gifItem = GifItem(gif)
-                        gifsItemAdapter.add(gifItem)
+                    client.search(query, MediaType.gif, 5, 0, null, null) { result, e ->
+                        if (result == null) {
+                            // Do what you want to do with the error
+                        } else {
+                            if (result.data != null) {
+//                                add gifs in arraylistof Gif model
+                                for (gif in result.data) {
+//                                    Log.v("giphy", gif.id)
+                                    arrayGifs.add(Gif(gif.images.fixedHeight.gifUrl))
+                                }
+
+//                                Display gifs in adapter
+                                for(gif in arrayGifs) {
+                                    val gifItem = GifItem(gif)
+                                    gifsItemAdapter.add(gifItem)
+                                }
+                            } else {
+                                Log.e("giphy error", "No results found")
+                            }
+                        }
                     }
-                } else {
-                    Log.e("giphy error", "No results found")
                 }
+                return false
             }
-        }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+//                Log.i("tag", newText)
+                return true
+            }
+        })
 
         //gestion du click event
         gifsItemAdapter.withOnClickListener(object: OnClickListener<GifItem> {
